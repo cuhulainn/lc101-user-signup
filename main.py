0a87cbf7f51a36ce499cef400c9a78980c1ef059
Connor McGinn <connor.mcginn@gmail.com>
2019-06-22 09:29:52 -0500

    added validation to input fields

---
+++ main.py
@@ -0,0 +1,54 @@
+from flask import Flask, request, redirect, render_template
+import cgi
+import os
+import re
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+@app.route("/")
+def index():
+    return render_template("signup.html", title = "User Signup")
+
+@app.route("/", methods=["POST"])
+def validate_input():
+    # Collect user inputs:
+    username = request.form['username']
+    password = request.form['password']
+    pwd_chk = request.form['ck_password']
+    email = request.form['email']
+
+    # Set up falsy user error variables:
+    username_error = ""
+    password_error = ""
+    pwd_match_error = ""
+    email_error = ""
+
+    # Validate username and clear value if invalid:
+    if not 3 <= len(username) <= 20:
+        username_error = "Your username must be between 3 and 20 characters."
+        username = ""
+    elif " " in username:
+        username_error = "Your username must not contain a space."
+        username = ""
+
+    # Validate password:
+    if not 3 <= len(password) <= 20:
+        password_error = "Your password must be between 3 and 20 characters."
+    elif " " in password:
+        password_error = "Your password must not contain a space."
+    
+    # Validate passwords match:
+    if not password == pwd_chk:
+        pwd_match_error = "Your passwords did not match, please type carefully!"
+    
+    
+    # Validate email:
+    #if email:
+
+    if not username_error and not password_error and not pwd_match_error and not email_error:
+        return render_template('welcome.html', username = username)
+    else:
+        return render_template('signup.html', username_error = username_error, password_error = password_error, pwd_match_error = pwd_match_error)
+        
+app.run()
